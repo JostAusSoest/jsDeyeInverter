7ce1893c69cb74c048cb11640f9f0e08d48795a3
Jost Schwider <2550433+JostAusSoest@users.noreply.github.com>
2023-10-17 18:28:14 +0200

    Create code.groovy
    V1

---
+++ code.groovy
@@ -0,0 +1,164 @@
+metadata
+{
+  definition(name:'jsDeyeInverter', namespace:'de.schwider', author:'Jost Schwider', description:'Just Simple Deye Inverter Driver')
+  {
+    capability 'Polling'
+    capability 'PowerMeter'
+    
+    attribute 'installedAt', 'String'
+    attribute 'updatedAt',   'String'
+    attribute 'isPolling',   'Boolean'
+    attribute 'power',       'Number'
+    attribute 'yieldToday',  'Number'
+    attribute 'yieldTotal',  'Number'
+  }
+  
+  preferences
+  {
+    input 'ipDevice',    'text',     title:'IP address:',     required:true,
+                                     description:'<small>' + (ipDevice ? "<a href='http://$ipDevice' target=_blank>Open Deye Inverter GUI</a> (needs power>0)" : 'E.g.: 192.168.178.66') + '</small>'
+    input 'user',        'text',     title:'Login user:',     required:true, defaultValue:'admin'
+    input 'password',    'password', title:'Login password:', required:true, defaultValue:'admin'
+    input 'autoPolling', 'bool',     title:'Auto polling',
+                                     description:'<small>Repeat poll every 5 minutes?<br />Necessary for regular power measurement.</small>'
+    input 'logReport',   'bool',     title:'Log Report'
+    input 'logWarnings', 'bool',     title:'Log Warnings'
+  }
+}
+
+
+public installed()
+{
+  log.info 'installed: ' + device
+  sendEvent name:'installedAt', value:tsFormat()
+}
+
+
+public void updated()
+{
+  log.info 'updated: ' + device
+  state.clear ()
+  sendEvent name:'updatedAt', value:tsFormat()
+  runIn 1, 'initPoller'
+}
+
+
+public void parse(def s)
+{
+  poll ()
+}
+
+
+// capability 'Polling': ====================
+
+
+public void poll()
+{
+  // Neuer Tag?
+  String ts = tsFormat()
+  if (ts[0..9] != device.currentUpdatedAt[0..9])
+  {
+    sendEvent name:'updatedAt', value:ts
+    sendEvent name:'yieldToday', value:0, unit:'kWh'
+  }
+  
+  // Aktuellen Status holen:
+  String s
+  use(groovy.time.TimeCategory)
+  {
+    // Abfrage nur, wenn Sonne aufgegangen (mit 30 Minuten Reserve):
+    def currTime = new Date()
+    if (location.sunrise - 30.minutes < currTime && currTime < location.sunset + 30.minutes)
+      s = requestStatus ()
+  }
+  
+  // Aktuelle Werte auslesen:
+  Float newPower = s ? value(s, 'webdata_now_p') : 0.0
+  if (newPower != device.currentPower)
+  {
+    sendEvent name:'updatedAt', value:ts
+    sendEvent name:'power', value:newPower, unit:'W'
+    if (newPower > 0)
+    {
+      sendEvent name:'yieldToday', value:value(s, 'webdata_today_e'), unit:'kWh'
+      sendEvent name:'yieldTotal', value:value(s, 'webdata_total_e'), unit:'kWh'
+    }
+  }
+}
+
+
+public void initPoller()
+{
+  unschedule ()
+  if (autoPolling) runEvery5Minutes 'poll'
+  sendEvent name:'isPolling', value:autoPolling
+  poll ()
+}
+
+
+// Interface Hubitat/DeyeInverter: ====================
+
+
+private String requestStatus()
+{
+  String url = 'http://' + ipDevice + '/status.html'
+  String s = httpRequest(url)
+  
+  if (logReport && s) log.info '<small>requestStatus: ' + s + '</small>'
+  
+  return s
+}
+
+
+private float value(String s, String name, Float defaultValue = 0)
+{
+  String v = substring(s, name + ' = "', '";')
+  if (v)
+    return (Float.parseFloat(v) * 10).intValue() / 10
+  else
+    return defaultValue
+}
+
+
+private String httpRequest(String url)
+{
+  def s = null
+  try
+  {
+    Map headers = [Authorization:'Basic ' + ("$user:$password").bytes.encodeBase64()]
+    Map params = [uri:url, headers:headers, timeout:15]
+    httpGet(params)
+    {
+      if (it.success) s = it.data
+    }
+  }
+  catch (Exception e)
+  {
+    if (logWarnings) log.warn 'httpRequest: ' + e
+    return null
+  }
+  return s.toString()
+}
+
+
+// Utilities: ====================
+
+
+private String tsFormat(String format = 'yyyy/MM/dd HH:mm:ss', Date ts = null)
+{
+  if (! ts) ts = new Date()
+  return ts.format(format)
+}
+
+
+private String substring(String s, String prefix, String postfix)
+{
+  int i = s.indexOf(prefix)
+  if (i < 0) return null
+  i += prefix.length()
+  
+  int j = s.indexOf(postfix, i)
+  if (j < 0) return null
+  
+  return s.substring(i, j)
+}
